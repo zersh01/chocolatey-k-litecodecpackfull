f39ac97fd6f9115c93bdd40a475eaf022bb84c54
zersh <zersh@mail.ru>
2015-11-02 13:46:11 +0300

    use helper, update package

--- tools/chocolateyUninstall.ps1
+++ tools/chocolateyUninstall.ps1
@@ -1,16 +1,15 @@
-$processor = Get-WmiObject Win32_Processor
-$is64bit = $processor.AddressWidth -eq 64
- 
 $packageName = 'k-litecodecpackfull'
 $fileType = 'exe'
 $silentArgs = '/SILENT /NORESTART'
-  
-if ($is64bit) {
+
+$osBitness = Get-ProcessorBits
+if ($osBitness -eq 64) 
+ {
   $uninstallString = (Get-ItemProperty HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\KLiteCodecPack_is1).UninstallString 
-} else {
+ } else {
   $uninstallString = (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\KLiteCodecPack_is1).UninstallString
-}
- 
+  }
+
 if ($uninstallString -ne "") {
     Uninstall-ChocolateyPackage $packageName $fileType $silentArgs $uninstallString
-}
+ }
