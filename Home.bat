284eddf555b84c9313410e4950f5e89522706d20
zersh <zersh@mail.ru>
2015-11-02 12:44:38 +0300

    Use rawgit.com for Icon

--- Home.bat
+++
@@ -1,9 +0,0 @@
-cls
-@echo off
-rem choco pack
-
-rem home
-choco install k-litecodecpackfull -source E:\Yandex_disk\backup\Chocolatey\MyProject\k-litecodecpackfull -y -force -version 11.5.5
-
-rem choco uninstall k-litecodecpackfull -a -y
-pause
--- work.bat
+++
@@ -1,7 +0,0 @@
-cls 
-@echo off
-choco pack
-rem choco install k-litecodecpackfull -source D:\YandexDisk\backup\Chocolatey\MyProject\k-litecodecpackfull -y -force -version 11.5.5
-
-rem choco uninstall k-litecodecpackfull -a -y
-pause
