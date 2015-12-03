153c983ad34ab041ecd47f05df1298cb82190c4d
zersh <zersh@mail.ru>
2015-12-03 15:18:16 +0300

    update package 1170

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,9 +1,10 @@
 ﻿$packageName = 'k-litecodecpackfull'
 $installerType = 'exe'
-$url = 'http://download.betanews.com/download/1094057842-3/K-Lite_Codec_Pack_1155_Full.exe'
+$url = 'http://download.betanews.com/download/1094057842-3/K-Lite_Codec_Pack_1170_Full.exe'
 $silentArgs = '/VERYSILENT'
 
-$checksum = '08469faa85296bd0ba3638b733e20321'
+$checksum = '300ee8fc0dd0b4663957e688104334be'
 $checksumType = 'md5'
  
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -Checksum "$checksum" -ChecksumType "$checksumType"
+
