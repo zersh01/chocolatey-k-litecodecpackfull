7c7e76a53816b0812f5ad5617c9044bd880d4fa8
zersh <zersh@mail.ru>
2016-03-03 10:01:36 +0300

    new version 12.0.1

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,12 +1,13 @@
 ﻿$packageName = 'k-litecodecpackfull'
 $installerType = 'exe'
-$url = 'http://download.betanews.com/download/1094057842-3/K-Lite_Codec_Pack_1190_Full.exe'
+$url = 'http://download.betanews.com/download/1094057842-3/K-Lite_Codec_Pack_1201_Full.exe'
 $silentArgs = '/VERYSILENT'
 
-$checksum = '7436420b2841c5de428abec6fa86a4ef'
+$checksum = '71f349acb9b23cc35d94ecf4994953d9'
 $checksumType = 'md5'
  
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -Checksum "$checksum" -ChecksumType "$checksumType"
+
 
 
 
--- tools/chocolateyUninstall.ps1
+++ tools/chocolateyUninstall.ps1
@@ -16,3 +16,4 @@ if ($uninstallString -ne "") {
 
 
 
+
