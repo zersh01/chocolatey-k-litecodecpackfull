93572c4f1a365f8e46a2616cdef00817a5a2f90c
Sergei Parhomenko <zersh@mail.ru>
2018-05-20 16:34:00 +0300

    remove old packages

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,13 +1,17 @@
 ﻿$packageName = 'k-litecodecpackfull'
 $installerType = 'exe'
-$url = 'http://files2.codecguide.com/K-Lite_Codec_Pack_1350_Full.exe'
-$silentArgs = '/VERYSILENT'
+$url = 'http://files2.codecguide.com/K-Lite_Codec_Pack_1415_Full.exe'
+$silentArgs = '/VERYSILENT /NORESTART'
                                          
-$checksum = 'a3d48ef7d6e6d65e9aab269e59735e01'
+$checksum = '974d795fa61ebf09cb613256a7b0a4ba'
 
 $checksumType = 'md5'
  
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -Checksum "$checksum" -ChecksumType "$checksumType"
+
+
+
+
 
 
 
--- tools/chocolateyUninstall.ps1
+++ tools/chocolateyUninstall.ps1
@@ -27,3 +27,7 @@ if ($uninstallString -ne "") {
 
 
 
+
+
+
+
